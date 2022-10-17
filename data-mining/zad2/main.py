25017faf15309c30359aa22df5f020b92e002908
m-czarnota <michal.czarnota.1998@gmail.com>
2022-10-17 11:47:33 +0200

    data mining zad 2 continues

--- data-mining/zad2/main.py
+++ data-mining/zad2/main.py
@@ -10,6 +10,8 @@ dla małych k dać krok mały, dla większych dać większy - do 20 co 2, do 50 
 
 
 import math
+import time
+
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats import truncnorm
@@ -26,27 +28,23 @@ def get_truncated_normal(mean=0, sd=1, low=0, upp=10):
 def generate_chessboard(n: int, gauss: bool = False):
     second_dim = math.sqrt(n)
 
-    X = np.random.rand(CHESSBOARD_SAMPLE_COUNT, 2)
+    x = np.random.rand(CHESSBOARD_SAMPLE_COUNT, 2)
 
-    Y = np.add(((np.floor(X[:, 0] * second_dim)) % second_dim), ((np.floor(X[:, 1] * second_dim)) % second_dim))
-    Y = Y % 2
+    y = np.add(((np.floor(x[:, 0] * second_dim)) % second_dim), ((np.floor(x[:, 1] * second_dim)) % second_dim))
+    y = y % 2
 
     if gauss is True:
         std = get_truncated_normal(low=0, upp=0.5)
-        X[:, 0] += std.rvs()
-        X[:, 1] += std.rvs()
+        x[:, 0] += std.rvs()
+        x[:, 1] += std.rvs()
 
-    return X, Y
+    return x, y
 
 
-if __name__ == '__main__':
-    [X, Y] = generate_chessboard(25, True)
-
-    # plt.scatter(X[:, 0], X[:, 1], c=Y)
-    # plt.show()
-
+def zad2(X, Y):
     k_dict = {}
     k = 1
+    k_step = 10
     max_score = 0
     k_optimal = 0
 
@@ -65,7 +63,7 @@ if __name__ == '__main__':
             max_score = new_score
             k_optimal = k
 
-        if i % 10 == 0:
+        if i % k_step == 0:
             k += 1
 
     print(max_score, k_optimal)
@@ -73,9 +71,112 @@ if __name__ == '__main__':
         k_dict[key] = np.mean(values)
 
     plt.figure()
-    plt.plot(k_dict.keys(), k_dict.values())
+    plt.scatter(k_dict.keys(), k_dict.values())
+    plt.title(f'score by k; k is avg for different data splits, k_step={k_step}; the best k={k_optimal}, max_score={max_score}')
     plt.xlabel('k')
     plt.ylabel('score')
     plt.grid()
     plt.show()
+
+
+def zad3(X, Y):
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.5)
+    k = 6
+    algorithms = ['brute', 'kd_tree', 'ball_tree']
+
+    times_of_fit = {}
+    times_of_score = {}
+    scores = []
+
+    for algorithm in algorithms:
+        neigh = KNeighborsClassifier(n_neighbors=k, algorithm=algorithm)
+
+        time1 = time.time()
+        neigh.fit(X_train, y_train)
+        time2 = time.time()
+        times_of_fit[algorithm] = time2 - time1
+
+        time1 = time.time()
+        score = neigh.score(X_test, y_test)
+        time2 = time.time()
+        times_of_score[algorithm] = time2 - time1
+        scores.append(score)
+
+    plt.figure()
+    plt.title(f'differences in time of fit by algorithm, k={k}')
+    plt.bar(times_of_fit.keys(), times_of_score.values(), label='time of fit by algorithm')
+    plt.show()
+
+    plt.figure()
+    plt.title(f'differences in time of score by algorithm, k={k}')
+    plt.bar(times_of_score.keys(), times_of_score.values())
+    plt.show()
+
+    plt.figure()
+    plt.title(f'differences in scores by algorithm, k={k}')
+    plt.bar(times_of_score.keys(), scores, label='score by algorithm')
+    plt.show()
+
+
+def zad4(X, Y):
+    algorithms = ['kd_tree', 'ball_tree']
+    k = 6
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.5)
+
+    for algorithm in algorithms:
+        times_of_fit = {}
+        times_of_score = {}
+        scores = []
+
+        for leaf_size in range(1, 60):
+            neigh = KNeighborsClassifier(n_neighbors=k, algorithm=algorithm, leaf_size=leaf_size)
+
+            time1 = time.time()
+            neigh.fit(X, Y)
+            time2 = time.time()
+            times_of_fit[leaf_size] = time2 - time1
+
+            time1 = time.time()
+            score = neigh.score(X_test, y_test)
+            time2 = time.time()
+            times_of_score[leaf_size] = time2 - time1
+            scores.append(score)
+
+        plt.figure()
+        plt.title(f'differences by leaf size for algorithm={algorithm}, k={k}')
+        plt.xlabel('leaf size')
+        plt.ylabel('time')
+        plt.plot(times_of_fit.keys(), times_of_fit.values(), label='time of fit by leaf size')
+        plt.plot(times_of_score.keys(), times_of_score.values(), label='time of score by leaf size')
+        plt.show()
+
+        plt.figure()
+        plt.title(f'differences in scores by leaf size for algorithm={algorithm}, k={k}')
+        plt.xlabel('leaf size')
+        plt.ylabel('score')
+        plt.scatter(times_of_score.keys(), scores, label='score by leaf size')
+        plt.show()
+
+
+def zad5(X, Y):
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.5)
+    # k = [1, 3, 11, 25]
+    k = [3]
+
+    for val_k in k:
+        neigh = KNeighborsClassifier(n_neighbors=val_k)
+        neigh.fit(X_train, y_train)
+
+        plt.figure()
+        plt.scatter(neigh.predict(X_test), y_test)
+        plt.show()
+
+
+if __name__ == '__main__':
+    [X, Y] = generate_chessboard(25)
+
+    # plt.scatter(X[:, 0], X[:, 1], c=Y)
+    # plt.show()
+
+    zad2(X, Y)
 
