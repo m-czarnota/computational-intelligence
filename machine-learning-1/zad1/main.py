1efe3f7d3ab3cad9bbe5b2200f2b5ca77f8c16a4
m-czarnota <michal.czarnota.1998@gmail.com>
2022-10-20 11:53:11 +0200

    machine learning 1 zad 1 update

--- machine-learning-1/zad1/main.py
+++ machine-learning-1/zad1/main.py
@@ -10,7 +10,7 @@ from scipy import sparse
 
 
 def freq(x, prob: bool = True) -> list:
-    if type(x) == sparse.csr.csr_matrix:
+    if type(x) == sparse.csr_matrix:
         x = x.data
 
     counts = {}
@@ -30,11 +30,9 @@ def freq(x, prob: bool = True) -> list:
     return [uniques, counts if prob is False else {key: val / total for key, val in counts.items()}]
 
 
-def freq2(x, y, prob: bool = True) -> list:
-    if type(x) == sparse.csr.csr_matrix:
-        x = x.data
-    if type(y) == sparse.csr.csr_matrix:
-        y = y.data
+def freq2(x, y, prob: bool = True):
+    if type(x) == sparse.csr_matrix or type(x) == sparse.csc_matrix:
+        return freq2_sparse(x, y, prob)
 
     counts = {}
     uniques = {'x': [], 'y': []}
@@ -56,6 +54,31 @@ def freq2(x, y, prob: bool = True) -> list:
 
     total = sum(counts.values())
     return [uniques['x'], uniques['y'], counts if prob is False else {key: val / total for key, val in counts.items()}]
+
+
+def freq2_sparse(x, y, prob: bool = True):
+    """
+    zrobić set(X), set(Y). cześć wspólna jako set.intersection(set)
+    """
+    x_nonzero = x.nonzero()[0]
+    y_nonzero = y.nonzero()[0]
+
+    uniques_x = set(x_nonzero)
+    uniques_y = set(y_nonzero)
+    intersection_x_y = uniques_x.intersection(uniques_y)
+
+    count_intersection = len(intersection_x_y)
+    count_x_nonzero = len(x_nonzero)
+    count_y_nonzero = len(y_nonzero)
+    count_shared_zeros = x.shape[0] - count_x_nonzero + y.shape[0] - count_y_nonzero
+
+    distribution_table = [
+        [count_shared_zeros, count_y_nonzero - count_intersection],
+        [count_x_nonzero - count_intersection, count_intersection]
+    ]
+    print(distribution_table)
+
+    return uniques_x, uniques_y, distribution_table if prob is False else [np.array(var) / (x.shape[0] + y.shape[0]) for var in distribution_table]
 
 
 def entropy(x, y=None, conditional_reverse: bool = False):
@@ -107,9 +130,28 @@ if __name__ == '__main__':
     print(sorted(info_gains.items(), key=lambda x: x[1], reverse=True))
 
     rcv1 = fetch_rcv1()
-    X = rcv1['data']
+    X = rcv1['data'] > 0
+    Xr = X[:, 2]
+    # print(X[:, 1], X[1, :])
     Y = rcv1['target'][:, 87]
 
-    print(type(Y), Y.data)
-    print(type(Y[260, 0]), Y[260, 0])
+    uniques_x, uniques_y, probs = freq2(Xr, Y)
+    print(entropy(Xr, Y))
+
+    # print(type(Y), Y.data)
+    # print(type(Y[260, 0]), Y[260, 0])
+
+    # print(rcv1.keys())
+    # print(rcv1['DESCR'])
+    # print(rcv1['target_names'])
+    # print(rcv1['sample_id'].shape)
+
+    """
+    wybrać jednego targeta, wybrać pojęcie które się zna. to będzie zmienna Y
+    zmienne warunkowe są w pliku data w zmiennej X.
+    słowa wczytać: word_list = pd.read_csv(..., delimiter=" ")
+    """
+
+    word_list = pd.read_csv('stem.termid.idf.map.txt', sep=' ')
+    # print(word_list['A'])
 
