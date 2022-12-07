3ed53ecb748d3bf7940e0c056b29069137f0df86
Czarny <michal.czarnota.1998@gmail.com>
2022-12-07 22:01:28 +0100

    complex network zad 5 IndependentCascadesModel modified

--- complex-network/zad3/main.py
+++ complex-network/zad3/main.py
@@ -6,7 +6,7 @@ from src.Model.IndependentCascadesModel2 import IndependentCascadesModel2
 if __name__ == '__main__':
     graph = nx.barabasi_albert_graph(16, 2)
 
-    model = IndependentCascadesModel2(len(graph.nodes), 0.2, [edge for edge in graph.edges], [1, 15])
+    model = IndependentCascadesModel2(len(graph.nodes), 0.21, [edge for edge in graph.edges], [1, 15])
     model.animation_interval__ = 1000
 
-    model.show_animation()
+    model.save_animation_to_file()
