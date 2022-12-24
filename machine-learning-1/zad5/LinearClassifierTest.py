283774b2ce8ddd18e5a41642da50b0f29ddf1ae5
czarpej <46748669+czarpej@users.noreply.github.com>
2022-12-24 15:27:36 +0100

    machine learning 1 zad 5 prepare to testing

--- machine-learning-1/zad5/LinearClassifierTest.py
+++ machine-learning-1/zad5/LinearClassifierTest.py
@@ -1,8 +1,12 @@
+from typing import Tuple
 import numpy as np
+import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
 from sklearn.svm import SVC
+from sklearn.datasets import fetch_rcv1, fetch_openml
+from sklearn.model_selection import GridSearchCV
 
 from Svm2 import Svm2
 
@@ -10,21 +14,60 @@ from Svm2 import Svm2
 class LinearClassifierTest:
     def __init__(self):
         self.test_sizes = np.logspace(-3, -0.5)
-        self.alphas = [10 ** i for i in range(-4, 0)]
+        self.regularization_params = [10 ** i for i in range(-3, 2)]
+        self.results_count_per_test_size = 100
 
-        self.classifiers = [SVC(kernel='linear'), Svm2(), MLPClassifier(hidden_layer_sizes=())]
-        self.classifiers.append(*[LogisticRegression(penalty=penalty) for penalty in ['l1', 'l2', 'elasticnet']])
-        for penalty in ['l1', 'l2', 'elasticnet']:
-            self.classifiers.append(LogisticRegression(penalty=penalty))
+        self.classifiers = {
+            'svc': GridSearchCV(SVC(kernel='linear'), param_grid={'C': self.regularization_params}),
+            'svm2': GridSearchCV(Svm2(), param_grid={'c': self.regularization_params}),
+            'mlp': GridSearchCV(MLPClassifier(hidden_layer_sizes=()), param_grid={'alpha': self.regularization_params}),
+            **{f'logistic_regression_{penalty}': GridSearchCV(LogisticRegression(penalty=penalty), param_grid={'C': self.regularization_params}) for penalty in ['l1', 'l2', 'elasticnet']}
+        }
 
         self.data_table = []
 
-    def experiment(self, x, y):
+    def experiment(self):
+        for method in [self.get_sonar_data, self.get_reuters_data, self.get_mnist_data]:
+            self.experiment_for_dataset(*method())
 
+    def experiment_for_dataset(self, x, y):
         for test_size in self.test_sizes:
-            for i in range(100):
-                separated_data = train_test_split(x, y, test_size=test_size, random_state=0)
+            results_storage = {clf_name: np.zeros(self.results_count_per_test_size, dtype='object') for clf_name in self.classifiers.keys()}
 
-                for alpha in self.alphas:
-                    for clf in self.classifiers:
-                        self.clf_experiment(clf, separated_data)
+            for results_iter in range(results_storage.size):
+                separated_data = train_test_split(x, y, test_size=test_size)
+
+                for clf_name, clf in self.classifiers.items():
+                    results_storage[clf_name][results_iter] = self.clf_experiment(clf, separated_data)
+
+    def clf_experiment(self, clf, separated_data: tuple) -> dict:
+        pass
+
+    def get_sonar_data(self) -> Tuple:
+        sonar_data = pd.read_csv('sonar_csv.csv')
+        y = self.normalize_decisions(sonar_data[sonar_data.columns[-1]])
+        x = sonar_data.drop(sonar_data.columns[-1], axis=1).to_numpy()
+
+        return x, y
+
+    @staticmethod
+    def get_reuters_data() -> Tuple:
+        rcv1 = fetch_rcv1()
+        x = rcv1['data'] > 0
+        xr = x[:, 2]
+        y = rcv1['target'][:, 5]
+
+        return xr, y
+
+    @staticmethod
+    def get_mnist_data() -> Tuple:
+        x, y = fetch_openml("mnist_784", version=1, return_X_y=True, as_frame=False, parser="pandas")
+
+        return x, y
+
+    @staticmethod
+    def normalize_decisions(d) -> np.array:
+        d_normalized = np.ones(d.size).astype("int8")
+        d_normalized[d == np.unique(d)[0]] = -1
+
+        return d_normalized
