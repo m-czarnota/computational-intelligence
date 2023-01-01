97030cba727b70554c491b75983dfb23ac980b08
Czarny <michal.czarnota.1998@gmail.com>
2023-01-01 15:44:15 +0100

    machine learning 2 reinforcement learning zad 2

--- machine-learning-2/reinforcement learning/zad2/main.py
+++ machine-learning-2/reinforcement learning/zad2/main.py
@@ -1,4 +1,7 @@
+from typing import Tuple
 import numpy as np
+from decimal import Decimal
+from matplotlib import pyplot as plt
 
 
 def zad1():
@@ -102,7 +105,7 @@ def zad3():
         values[10] = [1/ilosc_akcji * (gamma * v[ile_zapałek_zostaje]) + 1/ilosc_akcji * (gamma * v[ile_zapałek_zostaje]) + 1/ilosc_akcji * (gamma * v[ile_zapałek_zostaje])]
         values[10] = [prawdopodobieństwo że ja wykonam akcje -1 dla 10 zapałek a przeciwnik wykona akcje -3 i zostanie 6 zapałek + akcja moja -1 a typa -2 i zostanie 7 zapałek + .., ...]
         values[3] = [
-            sum(zrobie akcje -1 a on zrobi -x to nie ma wzmocnienia bo jego głupi ruch) dla x in range(-3, -1) - nieważne, czy zrobi ruch -2 czy -3, to jest jako jeden ruch, więc ilośc ruchów 1/2,
+            sum(zrobie akcje -1 a on zrobi -x to nie ma wzmocnienia bo jego głupi ruch) dla x in range[-3, -1) - nieważne, czy zrobi ruch -2 czy -3, to jest jako jeden ruch, więc ilośc ruchów 1/2,
             zrobie akcje -2 to nie ważne co on zrobi bo ja doprowadzam do wygranej i jest wzmocnienie,
             zrobie akcje -3 to przegrałem i nie ma więcej ruchów więc jest ruch 1
         ]
@@ -143,7 +146,97 @@ def zad4():
     mogą być problemy numeryczne
     najwieksza możliwa dokładność
     """
+    gamma = 0.9
+    win_prob = 18 / 37
+    state_count = 100
+    states = np.arange(state_count + 1)
+    how_many_we_want_win = 100
+
+    epsilon = 10e-6
+    bellman_loop_count_iter = 100
+    v = [Decimal(0) for _ in range(state_count + 1)]
+    vv = [Decimal(0) for _ in range(state_count + 1)]
+
+    values = np.empty(state_count + 1, dtype='object')
+    best_actions = np.empty(state_count + 1, dtype='object')
+    actions_by_state = np.empty(states.size, dtype='object')
+
+    for state_iter, state in enumerate(states):
+        actions_by_state[state_iter] = np.arange(1, np.min((state, state_count - state)) + 1)
+
+    for _ in range(bellman_loop_count_iter):
+        for state_iter, state in enumerate(states):
+            v_for_state = []
+
+            for action in actions_by_state[state_iter]:
+                state_y_win = state + action
+                state_y_lose = state - action
+
+                enhancement_for_win = 0 if state_y_win != how_many_we_want_win else 1
+                enhancement_for_lose = 0 if state_y_lose != how_many_we_want_win else 1
+
+                win_val = Decimal(win_prob) * (Decimal(enhancement_for_win) + Decimal(gamma) * v[state_y_win])
+                lose_val = Decimal(1 - win_prob) * (Decimal(enhancement_for_lose) + Decimal(gamma) * v[state_y_lose])
+
+                v_for_state.append(win_val + lose_val)
+
+            values[state_iter] = v_for_state
+
+        for k in range(len(vv)):
+            index, value = get_argmax_and_max_from_decimals(values[k])
+
+            best_actions[k] = index + 1 if np.where(values[k] == value)[0].size != actions_by_state[k].size else 0
+            vv[k] = value
+
+        if get_max_absolute_val_from_decimals(subtract_two_decimals_lists(v, vv)) < epsilon:
+            break
+
+        v[:] = vv[:]
+
+    plt.figure(figsize=(20, 10))
+    plt.plot(np.arange(len(v)), v)
+    plt.title("Optimal value function V*(x)")
+    plt.xlabel('State x (fund of player)')
+    plt.ylabel('The best value function V*(x)')
+    plt.savefig('optimal_value_function_v.png')
+    # plt.show()
+
+    plt.figure(figsize=(20, 10))
+    plt.scatter(np.arange(len(v)), best_actions)
+    plt.title("Optimal strategy (0 means that your it doesn't matter - all have the same effect)")
+    plt.xlabel('State x (fund of player)')
+    plt.ylabel('The best action to do')
+    plt.savefig('optimal_strategy.png')
+    # plt.show()
+
+
+def get_argmax_and_max_from_decimals(values: list) -> Tuple:
+    index_max = 0
+    val_max = Decimal(0)
+
+    for val_iter, val in enumerate(values[1:]):
+        if val > val_max:
+            val_max = val
+            index_max = val_iter
+
+    return index_max, val_max
+
+
+def subtract_two_decimals_lists(values1: list, values2: list) -> list:
+    values = []
+    for val1_iter, val1 in enumerate(values1):
+        val2 = values2[val1_iter]
+        values.append(val1 - val2)
+
+    return values
+
+
+def get_max_absolute_val_from_decimals(values: list) -> Decimal:
+    values_abs = list(map(lambda val: abs(val), values))
+    val_max = max(values_abs)
+
+    return val_max
 
 
 if __name__ == '__main__':
-    zad3()
+    zad4()
