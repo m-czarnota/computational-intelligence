953de5bcc0b26751cf43857eeb666c44cf6193a9
m-czarnota <michal.czarnota.1998@gmail.com>
2023-01-19 11:50:30 +0100

    machine learning 1 zad 6

--- machine-learning-1/zad6/RegressionClassifierTest.py
+++ machine-learning-1/zad6/RegressionClassifierTest.py
@@ -1,14 +1,20 @@
+import time
 from typing import Tuple
 import numpy as np
+from sklearn.metrics import mean_absolute_error, mean_squared_error
 from sklearn.base import RegressorMixin
-from sklearn.linear_model import LinearRegression, Huber, Ridge, ElasticNet, SGDRegressor, HuberRegressor
+from sklearn.linear_model import LinearRegression, Ridge, ElasticNet, SGDRegressor, HuberRegressor, Lasso
 from sklearn.model_selection import train_test_split
 from matplotlib import pyplot as plt
+from sklearn.neural_network import MLPRegressor
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import PolynomialFeatures, StandardScaler
 
 
 class RegressionClassifierTest:
-    def __init__(self):
-        self.noise_factor: float = 0.2
+    def __init__(self, noise_factor: float = 0.2, max_iter: int = 1000):
+        self.noise_factor: float = noise_factor
+        self.max_iter: int = max_iter
 
     def experiment(self, dataset_type: str = 'linear', strong_noise: bool = False, dataset_size: int = 100, polynomial_degree: int = 3):
         dataset_params = {'size': dataset_size, 'strong_noise': strong_noise}
@@ -21,22 +27,30 @@ class RegressionClassifierTest:
         regressors = self.get_regressors_for_linear_experiment() if dataset_type == 'linear' else self.get_regressors_for_non_linear_experiment()
 
         plt.figure()
-        plt.scatter(x_train, y_train)
+        plt.scatter(x, y)
 
         for regressor in regressors:
-            self.experiment_for_regressor(regressor, data=(x_train, x_test, y_train), plot=True)
+            self.experiment_for_regressor(regressor, data=(x, y), plot=True)
 
         plt.legend()
         plt.show()
 
     @staticmethod
     def experiment_for_regressor(regressor: RegressorMixin, data: tuple, plot: bool = True):
-        x_train, x_test, y_train = data
-        regressor.fit(x_train.reshape(-1, 1), y_train)
+        model = make_pipeline(PolynomialFeatures(15), regressor)
+        scaler = StandardScaler()
+
+        x, y = list(map(lambda coordinates: scaler.fit_transform(coordinates.reshape(-1, 1)), data))
+
+        fit_time1 = time.time()
+        model.fit(x, y)
+        fit_time2 = time.time()
+        fit_time = fit_time2 - fit_time1
 
         if plot:
-            y_predicted = regressor.predict(x_test.reshape(-1, 1))
-            plt.plot(x_test, y_predicted, label=regressor.__class__.__name__)
+            y_predicted = model.predict(x)
+            mse = mean_squared_error(y_predicted, y)
+            plt.plot(x, y_predicted, label=regressor.__class__.__name__)
 
     def generate_linear_dataset(self, size: int = 100, strong_noise: bool = False) -> Tuple:
         x = np.linspace(-2, 2, size)
@@ -49,7 +63,14 @@ class RegressionClassifierTest:
 
     def generate_non_linear_dataset(self, size: int = 100, strong_noise: bool = False, polynomial_degree: int = 3) -> Tuple:
         x = np.linspace(-2, 2, size)
-        y = np.vectorize(self.generate_noise)(x ** polynomial_degree)
+
+        y_fragments = [x ** degree for degree in range(polynomial_degree, -1, -1)]
+        y = y_fragments[0]
+
+        for y_fragment in y_fragments[1:]:
+            y += y_fragment
+
+        y = np.vectorize(self.generate_noise)(y)
 
         if strong_noise:
             y = self.add_strong_noise(y)
@@ -67,22 +88,23 @@ class RegressionClassifierTest:
 
         return data
 
-    @staticmethod
-    def get_regressors_for_linear_experiment() -> list:
+    def get_regressors_for_linear_experiment(self) -> list:
         return [
             LinearRegression(),
-            HuberRegressor(),
-            Ridge(),
-            ElasticNet(),
-            SGDRegressor(),
+            HuberRegressor(epsilon=5, max_iter=self.max_iter),
+            Ridge(max_iter=self.max_iter),
+            ElasticNet(max_iter=self.max_iter),
+            # SGDRegressor(penalty='l1', max_iter=self.max_iter),
+            # Lasso(max_iter=self.max_iter),
         ]
 
-    @staticmethod
-    def get_regressors_for_non_linear_experiment() -> list:
+    def get_regressors_for_non_linear_experiment(self) -> list:
         return [
             LinearRegression(),
-            HuberRegressor(),
+            HuberRegressor(epsilon=5, max_iter=self.max_iter),
             Ridge(),
             ElasticNet(),
-            SGDRegressor(),
+            # SGDRegressor(penalty='l2', max_iter=self.max_iter),
+            # Lasso(max_iter=self.max_iter),
+            # MLPRegressor(),
         ]
