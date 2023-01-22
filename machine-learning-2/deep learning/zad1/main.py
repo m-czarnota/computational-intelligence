143f6f39f288f223ee1bfc5d4004dfed652c69df
Czarny <michal.czarnota.1998@gmail.com>
2023-01-22 17:57:44 +0100

    machine learning 2 deep learning zad 1 experiment without image

--- machine-learning-2/deep learning/zad1/main.py
+++ machine-learning-2/deep learning/zad1/main.py
@@ -3,11 +3,11 @@ import numpy as np
 from keras.datasets import mnist
 from keras.utils import to_categorical
 from keras.models import Sequential
-from keras.layers import Dense
+from keras.layers import Dense, Dropout
 from keras.layers import Flatten
-from keras.optimizers import SGD
 from keras.optimizers import RMSprop
 import cv2
+from sklearn.model_selection import train_test_split
 
 IMAGES_DIR = './images'
 
@@ -26,55 +26,104 @@ def normalize_data(data: np.array) -> np.array:
 
 
 def add_noise_to_image(img: np.array, noise: float) -> np.array:
-    return img + noise * np.random.randn(28, 28)
+    return img + noise * np.random.randn(*img.shape)
 
 
 def rotate_image_by_90_degree(img: np.array) -> np.array:
     return np.rot90(img)
 
 
-def move_image_axes(img: np.array, delta: np.array = None) -> np.array:
-    if delta is None:
-        delta = np.arange(-5, 5, 0.1)
-
+def move_image_axes(img: np.array, delta: int = 2) -> np.array:
     img = np.roll(img, delta, axis=0)
     img = np.roll(img, delta, axis=1)
 
     return img
 
 
-def zad1(data: tuple):
-    x_train, y_train, x_test, y_test = data
+def zad1():
     activation_functions = ['sigmoid', 'hard_sigmoid', 'tanh', 'linear', 'relu', 'softmax']
 
-    model = Sequential()
-    model.add(Flatten())
-
-    opt = SGD(lr=0.01, momentum=0.9)
-
     for activation_function in activation_functions:
-        model.add(Dense(10, activation=activation_function, input_shape=(28 * 28, 1)))
-        model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
+        model = Sequential()
+        model.add(Flatten(input_shape=(28, 28)))
+        model.add(Dropout(0.2))
 
+        model.add(Dense(128, activation=activation_function))
+        model.compile(loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+        print(f'\nactivation function: {activation_function}')
+        model.fit(x_train, y_train)
+        res = model.evaluate(x_test, y_test)
+        print(f'res = {res}')
+
+        # model.predict(image)
+
+
+def zad2():
+    fit_epoch_counts = [10, 100, 1000]
+
+    for count in fit_epoch_counts:
+        model = Sequential()
+        model.add(Flatten(input_shape=(28, 28)))
+        model.add(Dropout(0.2))
+        model.add(Dense(128, activation='sigmoid'))
+
+        model.compile(loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+        print(f'\nfit epoch count: {count}')
+        model.fit(x_train, y_train, epochs=count)
         res = model.evaluate(x_test, y_test)
         print(f'res = {res}')
 
 
+def zad3():
+    optimizers = ['adam', 'sgd', 'adadelta', 'adagrad', 'rmsprop']
+
+    for optimizer in optimizers:
+        model = Sequential()
+        model.add(Flatten(input_shape=(28, 28)))
+        model.add(Dropout(0.2))
+        model.add(Dense(128, activation='sigmoid'))
+
+        model.compile(optimizer=optimizer, loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+        print(f'\noptimizer: {optimizer}')
+        model.fit(x_train, y_train, epochs=10)
+        res = model.evaluate(x_test, y_test)
+        print(f'res = {res}')
+
+
+def zad4():
+    learning_rates = [0.0001, 0.001, 0.005, 0.01, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8]
+
+    for learning_rate in learning_rates:
+        opt = RMSprop(learning_rate=learning_rate)
+
+        model = Sequential()
+        model.add(Flatten(input_shape=(28, 28)))
+        model.add(Dropout(0.2))
+        model.add(Dense(128, activation='sigmoid'))
+
+        model.compile(optimizer=opt, loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+        print(f'\nLearning rate: {learning_rate}')
+        model.fit(x_train, y_train, epochs=10)
+        res = model.evaluate(x_test, y_test)
+        print(f'res = {res}')
+
 
 if __name__ == '__main__':
-    mnist_data = mnist.load_data()
-    adjusted_data = [adjust_data(x, y) for x, y in mnist_data]
-    x_train, y_train, x_test, y_test = [normalize_data(column) for data in adjusted_data for column in data]
-
-    model = Sequential()
-    model.add(Flatten())
-    model.add(Dense(10, activation='softmax', input_shape=(28 * 28, 1)))
-
-    opt = SGD(lr=0.01, momentum=0.9)
-    model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
-
-    res = model.evaluate(x_test, y_test)
-    print(f'res = {res}')
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    x_train, x_test = x_train / 255.0, x_test / 255.0
 
     image = cv2.imread(f'{IMAGES_DIR}/sample_image.png', cv2.IMREAD_GRAYSCALE)
+    image = add_noise_to_image(image, 54)
+    image = rotate_image_by_90_degree(image)
+    image = move_image_axes(image)
 
+    image = cv2.resize(image, dsize=(28, 28), interpolation=cv2.INTER_CUBIC)
+
+    zad1()
+    # zad2()
+    # zad3()
+    # zad4()
