9c083a05c84e6de0f92972e8a8247bee666eb1de
m-czarnota <michal.czarnota.1998@gmail.com>
2023-03-06 09:58:09 +0100

    acoustic signals zad 1 change amplitude to db

--- acoustic-signals/zad1/main.py
+++ acoustic-signals/zad1/main.py
@@ -19,15 +19,43 @@ def draw_signal_amplitude(left: np.array, right: np.array) -> None:
     plt.show()
 
 
-def draw_signal_db(left: np.array, right: np.array) -> None:
+def draw_signal_db(left_channel: np.array, right_channel: np.array) -> None:
+    left_preview_db = 10 * np.log10(left_channel / max_freq_left)
+    right_preview_db = 10 * np.log10(right_channel / max_freq_right)
+
+    left_min_db = np.nanmin(left_preview_db[left_preview_db != -np.inf])
+    right_min_db = np.nanmin(right_preview_db[right_preview_db != -np.inf])
+
+    left_db_minus = np.ones(left_channel.shape) * left_min_db
+    left_db_plus = np.ones(left_channel.shape) * left_min_db
+
+    right_db_minus = np.ones(left_channel.shape) * right_min_db
+    right_db_plus = np.ones(left_channel.shape) * right_min_db
+
+    for amp_iter, (amp_left, amp_right) in enumerate(zip(left_channel, right_channel)):
+        amp_left_db = 10 * np.log(amp_left / max_freq_left)
+        amp_right_db = 10 * np.log(amp_right / max_freq_right)
+
+        if amp_left >= 0:
+            left_db_plus[amp_iter] = amp_left_db
+        else:
+            left_db_minus[amp_iter] = amp_left_db
+
+        if amp_right >= 0:
+            right_db_plus[amp_iter] = amp_right_db
+        else:
+            right_db_minus[amp_iter] = amp_right_db
+
     ax = plt.subplot(2, 1, 1)
-    ax.plot(np.linspace(0, signal_length, left.shape[0]), left)
+    ax.plot(np.linspace(0, signal_length, left_channel.shape[0]), left_db_minus)
+    ax.plot(np.linspace(0, signal_length, left_channel.shape[0]), left_db_plus)
     ax.set_title('Left channel')
     ax.set_xlabel('Time')
     ax.set_ylabel('dB')
 
     ax = plt.subplot(2, 1, 2)
-    ax.plot(np.linspace(0, signal_length, right.shape[0]), right)
+    ax.plot(np.linspace(0, signal_length, right_channel.shape[0]), right_db_minus)
+    ax.plot(np.linspace(0, signal_length, right_channel.shape[0]), right_db_plus)
     ax.set_title('Right channel')
     ax.set_xlabel('Time')
     ax.set_ylabel('dB')
@@ -93,12 +121,12 @@ if __name__ == '__main__':
     max_freq_right = np.max(data[:, 1])
     min_freq_right = np.min(data[:, 1])
 
-    draw_signal_amplitude(data[:, 0] / max_freq_left, data[:, 1] / max_freq_right)
+    # draw_signal_amplitude(data[:, 0] / max_freq_left, data[:, 1] / max_freq_right)
 
-    # draw_signal_db(
-    #     20 * np.log10(data[:, 0] / max_freq_left),
-    #     20 * np.log10(data[:, 1] / max_freq_right)
-    # )
+    draw_signal_db(
+        data[:, 0],
+        data[:, 1]
+    )
     # draw_signal_db(
     #     20 * np.log10(data[:, 0] / -0.3),
     #     20 * np.log10(data[:, 1] / -0.3)
@@ -109,13 +137,14 @@ if __name__ == '__main__':
     # )
 
     monofonic_signal = monofonic_signal_by_mean(data)
-    draw_monofonic_signal(monofonic_signal)
+    # draw_monofonic_signal(monofonic_signal)
 
     signal_with_removed_const_value = remove_const_value(data)
-    draw_signal_amplitude(signal_with_removed_const_value[:, 0], signal_with_removed_const_value[:, 1])
+    # draw_signal_amplitude(signal_with_removed_const_value[:, 0], signal_with_removed_const_value[:, 1])
 
 
 """
+normalizujemy do wartołści maksymalnej i jeszcze potem do zadanej wartości
 składowa stała - wartość średnia sygnału, sygnał nie jest rozłożony równomiernie wzdłuż osi x
 
 zad 3
