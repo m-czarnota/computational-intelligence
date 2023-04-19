757c1354b94717b7ad9dad25a82c652e19b01ddc
Czarny <michal.czarnota.1998@gmail.com>
2023-04-19 22:14:12 +0200

    acoustic signals zad 4 finish

--- acoustic-signals/zad4/FilterVisualizer.py
+++ acoustic-signals/zad4/FilterVisualizer.py
@@ -6,28 +6,38 @@ from Filter import Filter
 
 class FilterVisualizer:
     @staticmethod
-    def visualise_amplitude(fir: Filter) -> None:
-        plt.figure()
-        plt.plot(np.arange(fir.max_freq), fir.get_amplitude_characteristic())
-        plt.title('Amplitude characteristics')
-        plt.xlabel('Hz')
-        plt.ylabel('H(jΩ)')
+    def visualise_amplitude(fir: Filter, params: dict = {}) -> None:
+        fig, ax = plt.subplots(figsize=(20, 10))
+        ax.plot(np.arange(fir.max_freq), fir.get_amplitude_characteristic())
+        ax.set_title('Amplitude characteristics')
+        ax.set_xlabel('Hz')
+        ax.set_ylabel('H(jΩ)')
+
+        if 'text' in params.keys():
+            ax.text(-0.15, 0.95, params['text'], transform=ax.transAxes, fontsize=10,
+                verticalalignment='top', bbox=dict(boxstyle='round', facecolor='green', alpha=0.5))
+
         plt.show()
-        plt.close()
+        plt.close(fig)
 
     @staticmethod
-    def visualise_phase(fir: Filter) -> None:
-        plt.figure()
-        plt.plot(np.arange(fir.max_freq), fir.get_phase_characteristic())
-        plt.title('Phase characteristics')
-        plt.xlabel('Hz')
-        plt.ylabel('H(jΩ)')
+    def visualise_phase(fir: Filter, params: dict = {}) -> None:
+        fig, ax = plt.subplots(figsize=(20, 10))
+        ax.plot(np.arange(fir.max_freq), fir.get_phase_characteristic())
+        ax.set_title('Phase characteristics')
+        ax.set_xlabel('Hz')
+        ax.set_ylabel('H(jΩ)')
+
+        if 'text' in params.keys():
+            ax.text(-0.15, 0.95, params['text'], transform=ax.transAxes, fontsize=10,
+                    verticalalignment='top', bbox=dict(boxstyle='round', facecolor='green', alpha=0.5))
+
         plt.show()
-        plt.close()
+        plt.close(fig)
 
     @staticmethod
     def visualize_amplitude_db(fir: Filter) -> None:
-        plt.figure()
+        plt.figure(figsize=(20, 10))
         plt.plot(np.full(fir.get_amplitude_characteristic_decibel().shape[0], fir.boundary_decibel))
         plt.plot(np.arange(fir.max_freq), fir.amplitude_characteristic_decibel)
         plt.scatter(fir.boundary_frequencies, fir.get_decibels_for_boundary_frequencies())
--- acoustic-signals/zad4/main.py
+++ acoustic-signals/zad4/main.py
@@ -1,15 +1,14 @@
 import numpy as np
 from matplotlib import pyplot as plt
-from scipy.io import wavfile
 from scipy.signal import firwin
 
 from Filter import Filter
 from FilterVisualizer import FilterVisualizer
-from Signal import Signal
 
 
 def generate_filter_coefficients(coefficient_number: int, fs: int, fc: int, window: str = 'hamming') -> np.array:
     """
+    :param window:
     :param coefficient_number:
     :param fs: sampling frequency
     :param fc: boundary frequency
@@ -18,32 +17,12 @@ def generate_filter_coefficients(coefficient_number: int, fs: int, fc: int, wind
     nyquist_rate = fs / 2
     cutoff_frequency = fc / nyquist_rate
 
-    return firwin(coefficient_number, cutoff_frequency, window=window)
-
-    coefficients = np.zeros(coefficient_number)
-
-    for i in range(coefficient_number):
-        nominator = np.sin(2 * np.pi * i * (fc / fs))
-        denominator = (i * np.pi)
-        coefficient = nominator / (denominator if denominator != 0 else 1)
-
-        coefficients[i] = coefficient
-
-    if window == 'hamming':
-        window_function = np.hamming
-    elif window == 'hanning':
-        window_function = np.hanning
-    else:
-        window_function = np.blackman
-
-    coefficients *= window_function(coefficient_number)
-
-    return coefficients
+    return firwin(coefficient_number, cutoff_frequency, window=window, pass_zero='lowpass')
 
 
-def calc_impulse_response(fir: np.array):
+def calc_impulse_response(fir: np.array, filter_params: dict):
     impulse_response = np.zeros(fir.shape)
-    cutoff_frequency = fc / fs
+    cutoff_frequency = filter_params["fc"] / filter_params["fs"]
 
     for i in range(fir.shape[0]):
         value = i + 1
@@ -54,33 +33,71 @@ def calc_impulse_response(fir: np.array):
         else:
             impulse_response[i] = 2 * np.pi * cutoff_frequency
 
-    plt.figure()
-    plt.plot(impulse_response)
-    plt.title('Filter impulse response')
-    plt.xlabel('Coefficient number')
-    plt.ylabel('Coefficient value')
+    fig, ax = plt.subplots(figsize=(20, 10))
+    ax.plot(impulse_response)
+    ax.set_title('Filter impulse response')
+    ax.set_xlabel('Coefficient number')
+    ax.set_ylabel('Coefficient value')
+    ax.text(-0.15, 0.95, filter_params['info'], transform=ax.transAxes, fontsize=10,
+            verticalalignment='top', bbox=dict(boxstyle='round', facecolor='green', alpha=0.5))
     plt.show()
-    plt.close()
+    plt.close(fig)
+
+
+def plots_visual_comparison() -> None:
+    fs = 48000
+    fc = 3800
+    coefficient_number = 1000
+
+    for window in ['blackman', 'hann', 'hamming']:
+        fir_coefficients = generate_filter_coefficients(coefficient_number, fs, fc, window=window)
+        info = '\n'.join((
+            f'window: {window}',
+            f'coefficients: {coefficient_number}',
+            f'fs: {fs}',
+            f'fc: {fc}',
+        ))
+
+        fig, ax = plt.subplots(figsize=(20, 10))
+        ax.plot(fir_coefficients)
+        ax.set_title('Generated filter')
+        ax.set_xlabel('Coefficient number')
+        ax.set_ylabel('Coefficient value')
+        ax.text(-0.15, 0.95, info, transform=ax.transAxes, fontsize=10,
+                verticalalignment='top', bbox=dict(boxstyle='round', facecolor='green', alpha=0.5))
+        plt.show()
+        plt.close(fig)
+
+        calc_impulse_response(fir_coefficients, {'fs': fs, 'fc': fc, 'info': info})
+
+        fir = Filter(fir_coefficients, int(fs / 2))
+        print(fir.get_params())
+
+        FilterVisualizer.visualise_amplitude(fir, {'text': info})
+        FilterVisualizer.visualise_phase(fir, {'text': info})
+
+
+def compare_characteristics() -> None:
+    fs = 48000
+    fc = 3800
+    window = 'hamming'
+
+    for coefficient_number in [100, 500, 1000, 2000, 5000]:
+        fir_coefficients = generate_filter_coefficients(coefficient_number, fs, fc, window=window)
+        info = '\n'.join((
+            f'window: {window}',
+            f'coefficients: {coefficient_number}',
+            f'fs: {fs}',
+            f'fc: {fc}',
+        ))
+
+        fir = Filter(fir_coefficients, int(fs / 2))
+        print(fir.get_params())
+
+        FilterVisualizer.visualise_amplitude(fir, {'text': info})
+        FilterVisualizer.visualise_phase(fir, {'text': info})
 
 
 if __name__ == '__main__':
-    fs = 48000
-    fc = 1500
-    coefficient_number = 1000
-
-    fir_coefficients = generate_filter_coefficients(coefficient_number, fs, fc)
-
-    plt.figure()
-    plt.plot(fir_coefficients)
-    plt.show()
-    plt.close()
-
-    calc_impulse_response(fir_coefficients)
-
-    fir = Filter(fir_coefficients, int(fs / 2))
-    print(fir.get_params())
-
-
-
-    FilterVisualizer.visualise_amplitude(fir)
-    FilterVisualizer.visualise_phase(fir)
+    plots_visual_comparison()
+    compare_characteristics()
